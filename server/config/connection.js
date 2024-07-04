29e80be900e32b02fe28e2ad04ead5cb0d8e2ae6
austinallen <austintallen07@gmail.com>
2024-07-04 03:35:15 -0400

    Update MongoDB URI and resolve deprecation warnings

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://user-123:Papa7758$@clusterreps.w6x6iki.mongodb.net/?retryWrites=true&w=majority&appName=ClusterReps', {
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://user-123:Papa7758$@ac-gbxv2gu-shard-00-00.w6x6iki.mongodb.net:27017,ac-gbxv2gu-shard-00-01.w6x6iki.mongodb.net:27017,ac-gbxv2gu-shard-00-02.w6x6iki.mongodb.net:27017/myDatabaseName?ssl=true&replicaSet=atlas-k5ulnb-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).catch(err => console.error('Error connecting to MongoDB:', err));
 
 module.exports = mongoose.connection;
