4722eaca17cf048b1e5e89362663be5386cc861f
austinallen <austintallen07@gmail.com>
2024-07-04 01:14:32 -0400

    changed the port to listen on 0.0.0.0 instead of the default

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ async function startServer() {
   });
 
   db.once('open', () => {
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
